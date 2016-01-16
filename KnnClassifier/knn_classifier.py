44b74e802466012e18d5bc3d903a8956c7c0a8e8
Joel Lassen <jmlassen@gmail.com>
2016-01-15 20:59:24 -0700

    Adding classifier tester...because it feels like we are separating responsibilities that way.

--- KnnClassifier/knn_classifier.py
+++ KnnClassifier/knn_classifier.py
@@ -1,10 +1,10 @@
 class KnnClassifier:
 
-    def __init__(n_neighbors=1):
+    def __init__(self, n_neighbors=1):
         pass
 
-    def fit(train_data, train_target):
+    def fit(self, train_data, train_target):
         pass
 
-    def predict(test_data):
+    def predict(self, test_data):
         return 0;
---
+++ KnnClassifier/knn_classifier_tester.py
@@ -0,0 +1,13 @@
+class KnnClassifierTester:
+    """Runs tests on a KnnClassifier object.
+    """
+
+    def test(self, classifier, test_data, test_target):
+        """Tests KnnClassifier with passed testing data. Returns accuracy, maybe more?
+
+        :param classifier:
+        :param test_data:
+        :param test_target:
+        :return:
+        """
+        return 0
