0af3297d9508ef9008b7534fb20c68430d333eea
Joel Lassen <jmlassen@gmail.com>
2016-01-15 18:33:13 -0700

    Added comment.

--- KnnClassifier/main.py
+++ KnnClassifier/main.py
@@ -1,7 +1,10 @@
 from sklearn import datasets
+from KnnClassifier.driver import Driver
 
 
 def main():
+    """Calls run methods in driver class.
+    """
     driver = Driver()
     driver.run_iris_classification(datasets.load_iris())
 
