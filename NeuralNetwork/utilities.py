08bd7c0577790576590a24fd33ff9884e38adab8
Joel Lassen <jmlassen@gmail.com>
2016-02-15 12:38:19 -0700

    Added sigmoid function.

--- NeuralNetwork/utilities.py
+++ NeuralNetwork/utilities.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 
 DEFAULT_WEIGHT_FLOOR = -1
@@ -6,3 +7,7 @@ DEFAULT_WEIGHT_CEILING = 1
 
 def get_starting_weights(n_inputs):
     return np.random.uniform(DEFAULT_WEIGHT_FLOOR, DEFAULT_WEIGHT_CEILING, n_inputs)
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp((-1 * x)))
