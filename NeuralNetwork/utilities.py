fa18539b6342669ae8fcc85969e06114c054a540
Joel Lassen <jmlassen@gmail.com>
2016-02-18 12:00:03 -0700

    Fixed tabbing issue.

--- NeuralNetwork/utilities.py
+++ NeuralNetwork/utilities.py
@@ -15,18 +15,18 @@ def sigmoid(x):
 
 
 def cross_val_score(classifier, data, target, cv):
-        data, target = shuffle(data, target)
-        fold_len = int(len(data) / cv)
-        results = []
-        for i in range(cv):
-            start_index = fold_len * i
-            end_index = fold_len * (i + 1)
-            train_data = np.concatenate((data[:start_index], data[end_index:]), axis=0)
-            train_target = np.concatenate((target[:start_index], target[end_index:]), axis=0)
-            classifier.fit(train_data, train_target)
-            prediction = classifier.predict(data[start_index:end_index])
-            if len(prediction) != len(target[start_index:end_index]):
-                raise Exception('Classifier predict function did not return correct number of results!')
-            accuracy = np.sum(prediction == target[start_index:end_index]) / len(prediction)
-            results.append(accuracy)
-        return np.array(results)
+    data, target = shuffle(data, target)
+    fold_len = int(len(data) / cv)
+    results = []
+    for i in range(cv):
+        start_index = fold_len * i
+        end_index = fold_len * (i + 1)
+        train_data = np.concatenate((data[:start_index], data[end_index:]), axis=0)
+        train_target = np.concatenate((target[:start_index], target[end_index:]), axis=0)
+        classifier.fit(train_data, train_target)
+        prediction = classifier.predict(data[start_index:end_index])
+        if len(prediction) != len(target[start_index:end_index]):
+            raise Exception('Classifier predict function did not return correct number of results!')
+        accuracy = np.sum(prediction == target[start_index:end_index]) / len(prediction)
+        results.append(accuracy)
+    return np.array(results)
